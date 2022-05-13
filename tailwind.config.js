936cd0cdce6e8d956fc8df9e90548edecb1e4b36
Jason Miller <jasonmiller.dev@gmail.com>
2022-05-13 21:52:25 +1000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./index.html", "./*.{html,js}"],
+  theme: {
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '30px',
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+  fontFamily: {
+    baloo: ["Baloo Bhai", "sans-serif"],
+  },
+}
